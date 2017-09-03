e4b8d0daa394936f9190aa0a8501c305b79dd7b7
dvreddy007 <dvreddy007@gmail.com>
2017-09-03 22:26:32 +0530

    committing all the files

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-# userregistrationlogin
-User registration and login with local storage of user details functionality
-User login form and register for the first time login users need to click on register 
- button for this used the local storage and till the localstorage clears the data remains stays at the localstorage and the registered users
- informations statys on the local storage
- 
- 
-
+User registration and login with local storage of user details functionality User login form and register for the first time login users need to click on register button for this used the local storage and till the localstorage clears the data remains stays at the localstorage and the registered users informations statys on the local storage
---
+++ register/userform.controller.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('UserformController', UserformController);
+
+    UserformController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
+    function UserformController(UserService, $location, $rootScope, FlashService) {
+        var vm = this;
+
+        vm.userform = userform;
+
+        function userform() {
+            vm.dataLoading = true;
+            UserService.Create(vm.user)
+                .then(function (response) {
+                    if (response.success) {
+                        FlashService.Success('User Signup successful', true);
+                        $location.path('/login');
+                    } else {
+                        FlashService.Error(response.message);
+                        vm.dataLoading = false;
+                    }
+                });
+        }
+    }
+
+})();
